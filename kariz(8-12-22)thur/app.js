1cef5639dea24b003acdd6a5f1cd4243a8460580
Karizali <96954751+Karizali@users.noreply.github.com>
2022-12-08 21:05:38 +0500

    Add files via upload

---
+++ kariz(8-12-22)thur/app.js
@@ -0,0 +1,28 @@
+// add any tag
+// var main=document.getElementById('main');
+// function add(){
+//     console.log('e')
+//     var paragragh=document.createElement("p");
+//     var text=document.createTextNode('A B C D');
+//     paragragh.appendChild(text);
+//     main.appendChild(paragragh);
+    
+// }
+
+
+
+// function noreload(e){
+//     e.preventDefault();
+// }
+// function list(){
+//     var inp=document.getElementById('inp');
+//     var ul=document.getElementById('ul');
+//     var li=document.createElement('li');
+//     var button =document .createElement('button');
+//     var litext=document.createTextNode(inp.value);
+//     li.appendChild(litext);
+//     li.appendChild(button);
+//     ul.appendChild(li);
+
+// }
+
